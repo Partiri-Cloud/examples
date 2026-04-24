6fdbc0b3bf93a0b365e8a376ff8a23df6c47f293
J040M <jpmachacaz@gmail.com>
2026-04-24 11:46:14 +0200

    Update dotnet examples

--- dotnet/hello-aspnet/Program.cs
+++ dotnet/hello-aspnet/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "3000";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
 builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
